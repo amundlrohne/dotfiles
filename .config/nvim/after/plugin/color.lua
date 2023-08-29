baf645224648d1adc36edecd0b2d0d27daab6bda
Amund Lunke Røhne <amundlrohne@gmail.com>
2023-08-29 13:43:01 +0200

    Add color lsp config

--- .config/nvim/after/plugin/color.lua
+++ .config/nvim/after/plugin/color.lua
@@ -5,8 +5,27 @@ require("catppuccin").setup({
 		treesitter = true,
 		harpoon = true,
 		mason = true,
+		lsp_trouble = true,
 		telescope = {
 			enabled = true,
+		},
+		native_lsp = {
+			enabled = true,
+			virtual_text = {
+				errors = { "italic" },
+				hints = { "italic" },
+				warnings = { "italic" },
+				information = { "italic" },
+			},
+			underlines = {
+				errors = { "underline" },
+				hints = { "underline" },
+				warnings = { "underline" },
+				information = { "underline" },
+			},
+			inlay_hints = {
+				background = true,
+			},
 		},
 	},
 })
