ab7d74c8bfa3c720d1f439d8bbe6a59f54fcb922
Amund Lunke Røhne <amund.rohne@mollie.com>
2025-08-21 15:08:06 +0200

    Fix feline

--- .config/nvim/lua/plugins/statusline.lua
+++ .config/nvim/lua/plugins/statusline.lua
@@ -2,8 +2,12 @@ return {
 	"feline-nvim/feline.nvim",
 	dependecies = { { "catppuccin" } },
 	config = function()
+		local ctp_feline = require("catppuccin.groups.integrations.feline")
+
+		ctp_feline.setup()
+
 		require("feline").setup({
-			components = require("catppuccin.groups.integrations.feline").get(),
+			components = ctp_feline.get_statusline(),
 		})
 	end,
 }
