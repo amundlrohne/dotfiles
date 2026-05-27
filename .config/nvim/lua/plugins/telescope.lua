30fa05b68abe8b84e6845fe021b35e9067d3b602
Amund Lunke Røhne <amundlrohne@gmail.com>
2026-05-27 12:11:51 +0200

    feat(telescope): better rg search

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,10 +4,21 @@ return {
 	dependencies = { { "nvim-lua/plenary.nvim" } },
 	config = function()
 		local builtin = require("telescope.builtin")
+		local function project_root()
+			return vim.fs.root(0, ".git") or vim.fn.getcwd()
+		end
+
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 		vim.keymap.set("n", "<leader>ps", function()
-			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+			builtin.live_grep({
+				cwd = project_root(),
+			})
+		end)
+		vim.keymap.set("x", "<leader>ps", function()
+			builtin.grep_string({
+				cwd = project_root(),
+			})
 		end)
 		vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
 	end,
